d71ff4e7d0040aa16173ac53c16c1e950bfeb69a
tamami <tamami.oka@gmail.com>
2021-12-03 14:35:55 +0700

    init commit

---
+++ src/main/kotlin/lab/phb/prediksia/PrediksiAApplication.kt
@@ -0,0 +1,11 @@
+package lab.phb.prediksia
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PrediksiAApplication
+
+fun main(args: Array<String>) {
+	runApplication<PrediksiAApplication>(*args)
+}
---
+++ src/test/kotlin/lab/phb/prediksia/PrediksiAApplicationTests.kt
@@ -0,0 +1,13 @@
+package lab.phb.prediksia
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PrediksiAApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
